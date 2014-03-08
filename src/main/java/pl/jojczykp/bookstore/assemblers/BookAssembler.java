a672a0942f93bfa89e46f4f9f542cbdce8a3b2f8
jojczykp <abc@def.ghi.jkl>
2014-03-08 21:01:43 +0100

    Commit file missing as result of packages renaming

---
+++ src/main/java/pl/jojczykp/bookstore/assemblers/BookAssembler.java
@@ -0,0 +1,40 @@
+package pl.jojczykp.bookstore.assemblers;
+
+import org.springframework.stereotype.Service;
+import pl.jojczykp.bookstore.commands.BookCommand;
+import pl.jojczykp.bookstore.entities.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookAssembler {
+
+	public List<BookCommand> toCommands(List<Book> domains) {
+		List<BookCommand> commands = new ArrayList<>(domains.size());
+		for (Book domain : domains) {
+			commands.add(toCommand(domain));
+		}
+
+		return commands;
+	}
+
+	private BookCommand toCommand(Book domain) {
+		BookCommand command = new BookCommand();
+		command.setChecked(false);
+		command.setId(domain.getId());
+		command.setVersion(domain.getVersion());
+		command.setTitle(domain.getTitle());
+
+		return command;
+	}
+
+	public Book toDomain(BookCommand command) {
+		Book domain = new Book();
+		domain.setId(command.getId());
+		domain.setVersion(command.getVersion());
+		domain.setTitle(command.getTitle());
+
+		return domain;
+	}
+}
